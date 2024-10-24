e14c372f5e7308e2df6fce3ada87912f4c1bf9a0
etonxonline <123908790+etonxonline@users.noreply.github.com>
2024-10-24 09:44:44 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+{
+  "jsonFile": "path/to/survey.json",
+  "googleScriptUrl": "https://script.google.com/macros/s/AKfycbyrcC2mO_FJJwPmk4x_O4xz5iLjYl3m73FByzYN3nwM6Zn1BGEmlKW2WRrpzFYSFc6v/exec",
+  "spreadsheetID": "1gOjtFLqz6k8qkv3o5BAAIE9pi5tul6d8urlcCNJ5QB8"
+}
